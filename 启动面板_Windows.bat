9b7ac10a719e8bdf85a5a6da545d2b583f2dbdc6
jj <459197014@qq.com>
2026-03-19 13:31:28 -0700

    Prune duplicate launcher scripts

--- 启动面板_Windows.bat
+++ 启动面板_Windows.bat
@@ -1,5 +1,9 @@
-@echo off
-cd /d "%~dp0"
-cd fingerprint-dashboard
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --app=http://localhost:3000 --window-size=1280,800
-npm run dev
+@echo off
+
+cd /d "%~dp0"
+
+cd fingerprint-dashboard
+
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --app=http://localhost:3000 --window-size=1280,800
+
+npm run dev
--- 日常启动面板.command
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-echo "🚀 正在为您极速启动指纹浏览器控制台..."
-./start.sh
